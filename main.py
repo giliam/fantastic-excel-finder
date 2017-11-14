185ff557fbe556f1661ca984dea72ee779ef8ce7
giliam <giliam@users.noreply.github.com>
2017-11-14 17:21:02 +0100

    Update main.py
    Removes condition supposed to make the program faster but forgets results.

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ for dirpath, _, filenames in os.walk('input/'):
         xlsx = pd.ExcelFile(os.path.join(dirpath, filename))
         for sheet_name in xlsx.sheet_names:
             sheet = pd.read_excel(xlsx, sheet_name, header=None)
-            if args.match in str(sheet):
-                for i, row in enumerate(sheet.index):
-                    for j, col in enumerate(sheet.columns):
-                        if args.match in str(sheet.get_value(row, col)):
-                            print("Found in", Fore.CYAN + sheet.get_value(row, col) + Style.RESET_ALL, "\n", Fore.YELLOW + "%s%s" % (chr(col+65), row+1) + Fore.WHITE, "\t\tsheet", Fore.GREEN + sheet_name + Fore.WHITE, "\t\tfile", Fore.RED + filename + Fore.WHITE)
+            for i, row in enumerate(sheet.index):
+                for j, col in enumerate(sheet.columns):
+                    if args.match in str(sheet.get_value(row, col)):
+                        print("Found in", Fore.CYAN + sheet.get_value(row, col) + Style.RESET_ALL, "\n", Fore.YELLOW + "%s%s" % (chr(col+65), row+1) + Fore.WHITE, "\t\tsheet", Fore.GREEN + sheet_name + Fore.WHITE, "\t\tfile", Fore.RED + filename + Fore.WHITE)
